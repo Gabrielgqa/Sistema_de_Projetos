4d53d8d8ef85a0d3c014b4d02e19d08b583185a8
GabrielGQA <gabrielgqa@gmail.com>
2019-05-17 00:18:08 -0300

    sistema de rotas

--- bootstrap.php
+++ bootstrap.php
@@ -2,4 +2,18 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-new GGQA\Framework\Router;
+$router = new GGQA\Framework\Router;
+
+$router->add('GET', '/', function(){
+    return 'Estamos na home';
+});
+
+$router->add('GET', '/projeto/(\d+)', function($params){
+    return 'Estamos mostrando o projeto de id: '. $params[1];
+});
+
+try {
+    echo $router->run();
+} catch (\GGQA\Framework\Exceptions\HttpException $e) {
+    echo json_encode(['error' => $e->getMessage()]);
+}
--- src/Router.php
+++ src/Router.php
@@ -2,7 +2,44 @@
 
     namespace GGQA\Framework;
 
-    class Router {
+    use GGQA\Framework\Exceptions\HttpException;
 
+    class Router 
+    {
+        private $routes = [];
+
+        public function add(string $method, string $pattern, $call)
+        {
+            $method = strtolower($method);
+            $pattern = '/^' . str_replace('/', '\/', $pattern) . '$/';
+            $this->routes[$method][$pattern] = $call;
+        }
+
+        public function run()
+        {
+            $url = $this->getCurrentUrl();
+            $method = strtolower($_SERVER['REQUEST_METHOD']);
+
+            if(empty($this->routes[$method])){
+                throw new HttpException('Página não encontrada', 404);
+            }
+
+            foreach ($this->routes[$method] as $route => $action) {
+                if(preg_match($route, $url, $params)){
+                    return $action($params);
+                }
+            }
+            
+            throw new HttpException('Página não encontrada', 404);
+        }
+
+        public function getCurrentUrl()
+        {
+            $url = $_SERVER['PATH_INFO'] ?? '/';
+            if(strlen($url) > 1){
+                $url = rtrim($url, '/');
+            }
+            return $url;
+        }
     }
 
